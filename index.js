178fe3a31d3c58d51d3bb47f5d6a4868739ef577
naatiworlds <natiworlds@gmail.com>
2024-08-22 22:57:21 +0200

    fix error routes

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ app.use(cors()); // Habilita CORS para todas las rutas
 app.use(express.json());
 
 
-// Conexión a MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => console.log("MongoDB conectado"))
-.catch(err => console.error(err));
+// // Conexión a MongoDB
+// mongoose.connect(process.env.MONGO_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// }).then(() => console.log("MongoDB conectado"))
+// .catch(err => console.error(err));
 
 app.get('/', (req, res) => {
     res.send('API está funcionando');
