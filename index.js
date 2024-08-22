84285deeebca1b412205b585245e1919e1c9ab21
naatiworlds <natiworlds@gmail.com>
2024-08-22 22:53:21 +0200

    fix error routes

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ mongoose.connect(process.env.MONGO_URI, {
 app.get('/', (req, res) => {
     res.send('API está funcionando');
 });
+const proyectosRoute = require('./routes/auth');
 
 app.use('/api/auth', authRoutes);
 
